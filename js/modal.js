ffbbc8585505811c5a2d7a4ec915a9f621fb2763
Emmanuel Caiola <emmanuel.caiola@free.fr>
2023-05-25 16:01:38 +0200

    variables

--- js/modal.js
+++ js/modal.js
@@ -39,7 +39,7 @@ const confirmInscription = document.querySelector(".confirmation");
 
 //fonction de validation adresse email
 function checkEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   
   return re.test(email);
 }
